9d857fc8425da04157fc60d9487538fc132e820a
fabrezi <fkschon@gmail.com>
2021-06-10 23:54:24 -0400

    Update eu03.java

--- java/eu03.java
+++ java/eu03.java
@@ -1,1 +1,23 @@
 
+public class eu03 {
+
+    static long primo(long n){
+        long factor = 1;
+        for(int i = 2; i <= n; i++){
+            if(n % i == 0){
+                 factor = i;
+
+                while(n % i == 0){
+                    n = (n/i);
+                }
+            }
+        }
+        return factor;
+
+    }
+
+    public static void main(String[] args){
+        long n = 600851475143L;
+        System.out.println(primo(n));
+    }
+}
