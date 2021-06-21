b74d32bece6abdda1d3e2a8f38ee07fa8bcc943f
fabrezi <fkschon@gmail.com>
2021-06-21 00:18:13 -0400

    Create eu03.c

---
+++ C/eu03.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+
+//code reuires work. breaks down at larger digits.//
+
+long primou(long n){
+    long factor = 1;
+    for(int i = 2; i <= n; i++){
+        if(n % 1 == 0){
+            factor = i;
+            
+            while (n% i == 0){
+                n = (n/i);
+            }
+        }
+    }
+    return factor;
+}
+
+int main(){
+    long n = 423;
+    std::cout<<"the largest prime factor is baby olalalal == "<< primou(n);
+}
