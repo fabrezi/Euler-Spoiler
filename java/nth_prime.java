0ebff60cacd67039619d8fe8da4c252845a06645
fabrezi <fkschon@gmail.com>
2021-07-07 17:28:42 -0400

    Create nth_prime.java

---
+++ java/nth_prime.java
@@ -0,0 +1,17 @@
+public class nth_prime {
+
+    //I: enter the nth term
+    //O: [nth]: n prime number!!
+
+    //prime counter method
+    static int primo(int n){
+
+    }
+    
+//main method
+    public static void main(String[] args){
+        int n = 100;
+        System.out.println("the nth term prime is:" + primo(n));
+
+    }
+}
