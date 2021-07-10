d9f28dec63770c178ac406c9ca3767b802aafb6e
fabrezi <fkschon@gmail.com>
2021-07-10 03:20:19 -0400

    Update nth_prime.java

--- java/nth_prime.java
+++ java/nth_prime.java
@@ -1,17 +1,28 @@
+//this only checks the n value and output whether
+//the value is prime or not
+
+
 public class nth_prime {
 
-    //I: enter the nth term
-    //O: [nth]: n prime number!!
+    public static boolean isprime(int n){
+        if(n<2){
+            return false;
+        }
 
-    //prime counter method
-    static int primo(int n){
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
 
+        return true;
     }
-    
-//main method
+
+    //main method
     public static void main(String[] args){
-        int n = 100;
-        System.out.println("the nth term prime is:" + primo(n));
+        //int counter = 0;
+        int n = 11;
+        System.out.println(isprime(n));
 
-    }
+   
 }
