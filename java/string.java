01bba0c84e90d6c026c2364ea13c122c8e5d1373
fabrezi <fkschon@gmail.com>
2021-08-10 21:05:43 -0400

    Create string.java

---
+++ java/string.java
@@ -0,0 +1,4 @@
+//this is a question from a code interview
+//create a string search method that can detect the number of times a target word occurs in a sentence
+//I: the cat and the dog ran on the street.
+//O: the: 3 times
