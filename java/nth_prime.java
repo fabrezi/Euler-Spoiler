c316e15694846e98c04fe4dde94e82aa8df573e8
unknown <fkschon@gmail.com>
2022-05-20 14:50:52 -0400

    slick

--- java/nth_prime.java
+++ java/nth_prime.java
@@ -1,28 +1,28 @@
-//this only checks the n value and output whether
-//the value is prime or not
-
-
-public class nth_prime {
-
-    public static boolean isprime(int n){
-        if(n<2){
-            return false;
-        }
-
-        for(int i = 2; i <= Math.sqrt(n); i++){
-            if(n % i == 0){
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    //main method
-    public static void main(String[] args){
-        //int counter = 0;
-        int n = 11;
-        System.out.println(isprime(n));
-
-   
-}
+////this only checks the n value and output whether
+////the value is prime or not
+//
+//
+//public class nth_prime {
+//
+//    public static boolean isprime(int n){
+//        if(n<2){
+//            return false;
+//        }
+//
+//        for(int i = 2; i <= Math.sqrt(n); i++){
+//            if(n % i == 0){
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//    }
+//
+//    //main method
+//    public static void main(String[] args){
+//        //int counter = 0;
+//        int n = 11;
+//        System.out.println(isprime(n));
+//
+//
+//}
--- java/string.java
+++ java/string.java
@@ -3,4 +3,20 @@
 //I: the cat and the dog ran on the street.
 //O: the: 3 times
 
+public class string{
+
+    public static void main(String[] args){
+        for(int i =0; i < 100; i++){
+            if(i%2==0){
+                System.out.println("here u go");
+            }
+            else{
+              //System.out.println(0);
+            }
+        }
+
+    }
+
+}
+
 
